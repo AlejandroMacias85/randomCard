04a85888a6c9ca8aab163fa8076662a40ea962fa
Luffy85 <macias85ale@gmail.com>
2022-06-15 22:54:20 +0000

    completado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,39 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  // write your code here
+  document.querySelector(".number").innerHTML = generateRandomNumbers();
+  let suit = generateRandomSuit();
+  document.querySelector(".top").innerHTML = suit;
+  document.querySelector(".bottom").innerHTML = suit;
+};
+let generateRandomSuit = () => {
+  let suit = ["♥️", "♦️", "♣️", "♠️"];
+  let indexSuit = Math.floor(Math.random() * suit.length);
+  if (suit[indexSuit] == "♥️" || suit[indexSuit] == "♦️") {
+    document.querySelector(".top").style.color = "red";
+    document.querySelector(".bottom").style.color = "red";
+  }
+  return suit[indexSuit];
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateRandomNumbers = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "k"
+  ];
+  let indexNumbers = Math.floor(Math.random() * numbers.length);
+  return numbers[indexNumbers];
 };
